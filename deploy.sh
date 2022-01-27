0dae0f536e0722e28753cbb3690a465bc4453627
RunSS11 <894985240@qq.com>
2022-01-27 17:30:21 +0800

    fix: action

--- deploy.sh
+++ deploy.sh
@@ -10,23 +10,24 @@ yarn build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-# echo 'b.XXXX.com' > CNAME
+# 如果发布到自定义域名，请打开并填写你的域名
+# echo 'www.xxx.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  # githubUrl=git@github.com:foreverRuns/vuepress-vdoing-blog.git
-  githubUrl=git@github.com:foreverRuns/blog.git
+  # githubUrl=https://github.com/foreverRuns/vuepress-vdoing-blog.git
+  githubUrl=https://github.com/foreverRuns/blog.git
 else
   msg='来自github actions的自动部署'
   # githubUrl=https://foreverRuns:${GITHUB_TOKEN}@github.com/foreverRuns/vuepress-vdoing-blog.git
   githubUrl=https://foreverRuns:${GITHUB_TOKEN}@github.com/foreverRuns/blog.git
-  git config --global user.name "foreverRuns"
+  git config --global user.name "RunSS11"
   git config --global user.email "894985240@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl main:gh-pages # 将工程下的main分支 推送到 blog仓库下的 gh-pages分支
 
 cd -
 rm -rf docs/.vuepress/dist

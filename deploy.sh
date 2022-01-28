21c45aee5d4861dd48779e2a73e542a4726ba952
RunSS11 <894985240@qq.com>
2022-01-28 13:26:20 +0800

    feat: action for gitee

--- deploy.sh
+++ deploy.sh
@@ -11,24 +11,25 @@ cd docs/.vuepress/dist
 
 # deploy to github pages
 # 如果发布到自定义域名，请使用，不填则使用 GitHub 自带的 <username>.github.io
-echo 'runs.com' > CNAME
+# echo 'blog.runs.cn' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  # githubUrl=https://github.com/foreverRuns/vuepress-vdoing-blog.git
-  githubUrl=https://github.com/foreverRuns/blog.git
+  msg='yarn deploy 本地手动部署'  #yarn deploy
+  # githubUrl=git@github.com:foreverRuns/vuepress-vdoing-blog.git
+  githubUrl=git@github.com:foreverRuns/blog.git
 else
-  msg='来自github actions的自动部署'
+  msg='GithuGb ActiAons 自动部署'
   # githubUrl=https://foreverRuns:${GITHUB_TOKEN}@github.com/foreverRuns/vuepress-vdoing-blog.git
   githubUrl=https://foreverRuns:${GITHUB_TOKEN}@github.com/foreverRuns/blog.git
   # Omit --global to set the identity only in this repository.
   git config --global user.name "RunSS11"
   git config --global user.email "894985240@qq.com"
+  # git branch -m main master
 fi
 git init 
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 将上面项目的master分支 推送到 blog仓库下的 gh-pages分支
+git push -f $githubUrl master:gh-pages # 将master分支 推送到 blog仓库下的 gh-pages分支
 
 # 退回开始所在目录
 cd -
